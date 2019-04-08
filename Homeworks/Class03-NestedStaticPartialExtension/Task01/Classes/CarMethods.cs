1399a340ada8403ae0f19a0fe1b710cb34e2825b
dariokostov <kostov.dario@gmail.com>
2019-04-08 23:43:51 +0200

    ok

---
+++ Homeworks/Class03-NestedStaticPartialExtension/Task01/Classes/CarMethods.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Task01.Classes
+{
+    public partial class Car
+    {
+        public Car(string brand, string model, string color, int fuel, bool keys, bool isDrivable)
+        {
+            this.Brand = brand;
+            this.Model = model;
+            this.Color = color;
+            this.Fuel = fuel;
+            this.HaveKeys = keys;
+            this.IsDrivable = isDrivable; // samo da se menjava
+        }
+
+        public void StartCar() 
+        {
+            if (HaveKeys)
+            {
+
+                    if(Fuel > 0)
+                    {
+                        Console.WriteLine($"The car {Brand} {Model} have keys is drivable and have fuel. Start driving!");
+                    }
+                    else
+                    {
+                        this.IsDrivable = false;
+                        Console.WriteLine($"The car {Brand} {Model} is ok, but don't have a fuel");
+                    }
+                
+            }
+            else
+            {
+                Console.WriteLine("No keys");
+            }
+        }
+
+        public void StartLights()
+        {
+        }
+    }
+}
---
+++ Homeworks/Class03-NestedStaticPartialExtension/Task01/Classes/CarProp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Task01.Classes
+{
+    public partial class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Fuel { get; set; }
+        public bool HaveKeys { get; set; }
+        public bool IsDrivable { get; set; }
+    }
+}
